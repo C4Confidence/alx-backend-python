5e048bdb59758ed5fdb6cc0ba603e9b9602c6414
C4Confidence <iamconfidence5@gmail.com>
2024-08-01 23:55:06 +0100

    advanced

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+from typing import List, Tuple, Union
+
+
+"""Use MYPY to validate the following piece of code"""
+
+
+def zoom_array(lst: Union[List[int], Tuple[int, ...]], factor: int = 2) -> List[int]:
+    """Corrected annotations"""
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array: List[int] = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
